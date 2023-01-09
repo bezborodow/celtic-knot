e6295b55734923f40653eb12a452e4ca194a17db
Damien Bezborodow <dbezborodov@gmail.com>
2023-01-10 02:31:08 +1030

    Reorder includes based on precedence.

--- lib/knot.rb
+++ lib/knot.rb
@@ -1,7 +1,7 @@
-require_relative "knot/point"
 require_relative "knot/grid"
-require_relative "knot/path"
 require_relative "knot/cell"
+require_relative "knot/point"
+require_relative "knot/path"
 
 module Knot
   class Error < StandardError; end
